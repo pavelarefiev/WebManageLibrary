716062f5e7a7a3902863a6cdbc5b8c012ca0f81c
pavelarefiev <apv.dr4ft5fd@gmail.com>
2024-12-14 23:39:09 +0700

    Версия способна вносить строчки в БД но не более того

--- WebApplication2/Pages/Books/Create.cshtml.cs
+++ WebApplication2/Pages/Books/Create.cshtml.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
 using WebApplication2.Pages.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System;
 
 namespace WebApplication2.Pages.Books
 {
@@ -20,7 +22,6 @@ namespace WebApplication2.Pages.Books
         public Book Book { get; set; }
 
         [BindProperty]
-        [Required]
         public IFormFile? UploadedFile { get; set; }
 
         public IActionResult OnGet()
@@ -34,8 +35,21 @@ namespace WebApplication2.Pages.Books
             {
                 return Page();
             }
+
             if (UploadedFile != null && UploadedFile.Length > 0)
             {
+                // Проверка типа файла
+                if (!UploadedFile.ContentType.Equals("application/pdf", StringComparison.OrdinalIgnoreCase))
+                {
+                    ModelState.AddModelError("File", "Файл должен быть PDF.");
+                    return Page();
+                }
+                // Проверка размера файла (не более 5Мб)
+                if (UploadedFile.Length > 5 * 1024 * 1024)
+                {
+                    ModelState.AddModelError("File", "Файл не должен превышать 5 Мб.");
+                    return Page();
+                }
                 string uploadsFolder = Path.Combine(_environment.WebRootPath, "uploads");
 
                 if (!Directory.Exists(uploadsFolder))
@@ -50,10 +64,17 @@ namespace WebApplication2.Pages.Books
                     await UploadedFile.CopyToAsync(stream);
                 }
                 Book.FilePath = "/uploads/" + fileName;
+
             }
+            else if (string.IsNullOrEmpty(Book.FilePath))
+            {
+                ModelState.AddModelError("Book.FilePath", "Введите путь к файлу или загрузите файл.");
+                return Page();
+            }
+
             _context.Books.Add(Book);
             await _context.SaveChangesAsync();
-            return RedirectToPage("./Index"); // перенаправить на страницу списка книг.
+            return RedirectToPage("/Index"); //  Изменено здесь
         }
     }
 }
